b9a561aa4720e9e47b6fce26661d6941e8732688
SantiAcevedo <eliasantiago.acevedo@gmail.com>
2024-08-22 23:52:05 -0300

    se agrego todo el juego

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# breakbrick.v2
+# Phaser Vite Template
+
+Juego de romper ladrillos con una pelota. 
+Para poder jugar a este, debes entrar a la consola (Ctrl + Ñ)
+Luego escribir npm run dev.
+Tocar el primer link (Ctrl + Click) y...
+Y listo ya podes jugar!!
---
+++ src/scenes/GameOver.js
@@ -0,0 +1,30 @@
+import { Scene } from 'phaser';
+
+export class GameOver extends Scene
+{
+    constructor ()
+    {
+        super('GameOver');
+    }
+
+    init(data) {
+        // Guardar el puntaje pasado desde la escena anterior
+        this.finalScore = data.finalScore || 0;
+      }
+    
+      create() {
+        const width = this.scale.width;
+        const height = this.scale.height;
+        
+        // Mostrar el mensaje de Game Over y el puntaje final
+        this.add.text(width / 2, height / 2 - 100, "GAME OVER", {
+          fontSize: "64px",
+          fill: "#fff"
+        }).setOrigin(0.5);
+    
+        this.add.text(width / 2, height / 2, `Score: ${this.finalScore}`, {
+          fontSize: "32px",
+          fill: "#fff"
+        }).setOrigin(0.5);
+      }
+    }
